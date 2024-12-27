1a2ea3d19edfffc44d7e2c76f592486e256e077c
Anmolkumar2002 <anmolkumar200210@gmail.com>
2024-12-27 11:42:09 +0530

    login page

--- EMP System/src/components/Auth/Login.jsx
+++ EMP System/src/components/Auth/Login.jsx
@@ -1,13 +1,40 @@
- import React from "react";
+ import React, { useState } from "react";
 
  const Login = () => {
+
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log("Email is", email);
+        console.log("Password is", password);
+        setEmail("")
+        setPassword("")
+    }
+
     return (
         <div className="flex h-screen w-screen items-center justify-center">
             <div className="border-2 border-emerald-600 p-20">
-                <form className="flex flex-col items-center justify-center">
-                    <input className="outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400" type="email" placeholder="Enter your Email" />
-                    <input className="outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl mt-3 placeholder:text-gray-400" type="password" placeholder="Enter your password" />
-                    <button className="text-white outline-none border-none bg-emerald-600 rounded-full py-3 px-5 text-xl mt-5 placeholder:text-gray-400">Log In</button>
+                <form 
+                onSubmit={(e) => {
+                    submitHandler(e)
+                }}
+                className="flex flex-col items-center justify-center">
+                    <input 
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                        required 
+                        className="outline-none bg-transparent border-2 border-emerald-600 font-medium rounded-full py-2 px-6 text-lg placeholder:text-gray-400" type="email" placeholder="Enter your email" />
+                    <input 
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        required className="outline-none bg-transparent border-2 border-emerald-600 font-medium rounded-full py-2 px-6 text-lg mt-3 placeholder:text-gray-400" type="password" placeholder="Enter your password" />
+                    <button className="mt-7 text-white outline-none border-none hover:bg-emerald-700 font-semibold bg-emerald-600 w-full rounded-full py-3 px-8 text-xl placeholder:text-white">Log In</button>
                 </form>
             </div>
         </div>
