6b43d6fa1544957f56f0c6c3ce78b4374961e6f9
Anmolkumar2002 <anmolkumar200210@gmail.com>
2025-02-02 15:22:13 +0530

    complete

--- TastyTrail/src/components/Banner/Banner.jsx
+++ TastyTrail/src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
     return (
         <>
             <div className='min-h-[550px]'>
-                <div className='min-h-[550px] flex justify-center items-center backdrop-blur-xl py-12 sm:py-0 '>
+                <div className=''>
                     <div data-aos="slide-up" data-aos-duration="300" className='container'>
                         <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
                             <div>
